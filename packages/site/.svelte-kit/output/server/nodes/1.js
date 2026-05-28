d9a70671e3cbe6d079e90372266e2bd6722e1a48
Julian <1041324235@qq.com>
2026-05-28 12:13:35 +0800

    chore: ignore .svelte-kit generated directory

--- packages/site/.svelte-kit/output/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.DkWoLYvp.js","_app/immutable/chunks/scheduler.CRzCUlSA.js","_app/immutable/chunks/index.BXd_S5Pc.js","_app/immutable/chunks/stores.Dpq44UO5.js","_app/immutable/chunks/entry.D6nKOufS.js","_app/immutable/chunks/index.-2gVFq1h.js"];
-export const stylesheets = [];
-export const fonts = [];
--- packages/site/.svelte-kit/output/server/nodes/2.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 2;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/_page.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/2.DfK-UB2f.js","_app/immutable/chunks/scheduler.CRzCUlSA.js","_app/immutable/chunks/each.CZ1pnjD_.js","_app/immutable/chunks/index.BXd_S5Pc.js","_app/immutable/chunks/lang.D_QClmZJ.js","_app/immutable/chunks/index.-2gVFq1h.js"];
-export const stylesheets = ["_app/immutable/assets/2.DmVWtVG1.css"];
-export const fonts = [];
